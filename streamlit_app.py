3ee3cff8406d19cbe6bb9e34bde52dc60e6b3b25
acaima <92732983+acaima@users.noreply.github.com>
2024-01-23 14:02:17 -0500

    Update streamlit_app.py
    import pandas library to import data frame

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,3 +3,7 @@ streamlit.title("My Parents New Healthy Diner")
 streamlit.header('Breakfast Menu')
 streamlit.text('Omega 3 & Blueberry Oatmeal')
 streamlit.text('Kale, Spinach & Rocket Smoothie') 
+
+import pandas
+my_fruit_list = pandas.read_csv('https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt')
+streamlit.dataframe(my_fruit_list)
