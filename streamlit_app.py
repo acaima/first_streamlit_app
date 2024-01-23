4a55ae7ca9623636bc1ccf5ec8252a1caaab5d28
acaima <92732983+acaima@users.noreply.github.com>
2024-01-23 13:46:47 -0500

    Update streamlit_app.py
    added line of contents

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,5 @@
 erimport streamlit
 streamlit.title("My Parents New Healthy Diner")
-streamlit.title("Breakfast Menu")
+streamlit.header("Breakfast Menu")
+streamlit.text("Omega 3 & Blueberry Oatmeal")
+streamlit.text("Kale, Spinach & Rocket Smoothie") 
